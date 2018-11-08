be7bdd2645ce2497a3d7c0538881ef499b9de4b3
Proxyte <segamegadrive18bit@gmail.com>
2018-11-08 10:31:05 +0600

    heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+  path = require('path');
+
+const app = express();
+
+app.use(express.static('./dist/kazakh-site'));
+
+app.get('/*',(req,res)=>{
+  res.sendFile(path.join('/dist/kazakh-site/index.html'));
+});
+
+app.listen(process.env.PORT || 8080,()=>{
+  console.log("server started");
+})
